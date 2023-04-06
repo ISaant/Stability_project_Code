8c0455185c76a4517d77a91889bd8e45e2cfe761
ISaant <sifa.sant@gmail.com>
2023-04-06 09:08:16 -0400

    Today commit

--- Fun2.py
+++ Fun2.py
@@ -25,13 +25,13 @@ ParentPath=os.path.abspath(os.path.join(current_path, os.pardir))
 class Generate_Data:
     
     band='alpha'
-    case=0.8
+    effectSize=0.8
     in_between=[1,40]
     def __init__(self):
         #Run Validation to the recived arguments
         
         assert isinstance(Generate_Data.band, str), f"Band value {Generate_Data.band} must be string"
-        assert Generate_Data.case == 0.2 or Generate_Data.case == 0.5 or Generate_Data.case == 0.8, "Posible cases are 0.2, 0.5 or 0.8"
+        assert Generate_Data.effectSize == 0.2 or Generate_Data.effectSize == 0.5 or Generate_Data.effectSize == 0.8, "Posible effectSizes are 0.2, 0.5 or 0.8"
         assert isinstance(Generate_Data.in_between, list),  "Between must be a list with 2 values [low freq, top freq]"
         assert np.array(Generate_Data.in_between).shape[0] == 2, "Between must be a list of len=2 with values [low freq, top freq]" 
         
@@ -39,22 +39,22 @@ class Generate_Data:
         self.path = ParentPath+'/Stability-project_db/simulated/'+Generate_Data.band+'/'
         
     def get_path (self):
-        print(self.path, '  +  case='+str(self.case)+'_250_PSD')
+        print(self.path, '  +  effectSize='+str(self.effectSize)+'_250_PSD')
     
-    def OpenCase (self):
-        casePath=self.path+str(Generate_Data.case)+'_250_PSD'
-        Dir=np.sort(os.listdir(casePath))[2:-2]
+    def Open_effectSize (self):
+        effectSizePath=self.path+str(Generate_Data.effectSize)+'_250_PSD'
+        Dir=np.sort(os.listdir(effectSizePath))[2:-2]
         self.Dir=Dir
 
-    def Generate_Window_idx (self, Windows=10,sampleSize=250):
+    def Generate_Window_idx (self, Windows=10,sampleSize=250,seed=21):
         #Run Validation to the recived arguments
         assert isinstance(Windows, int) and Windows <=100, "Windows must be a single integer between 1 and 100" 
         assert isinstance(sampleSize, int) and sampleSize <= 250, "sampleSize must be a single integer between 1 and 250"
-        # seed=np.random.seed(21)
+        seed=np.random.seed(seed)
         self.timeWindows=np.random.choice(self.Dir,size=Windows,replace=False) #iterar esto primero
         self.idx=np.sort(np.random.choice(np.arange(0,250),size=sampleSize,replace=False))
         #
-        # print(self.OpenCase())
+        # print(self.OpeneffectSize())
         
         # return seed, self.timeWindows, self.idx
         
@@ -63,7 +63,7 @@ class Generate_Data:
         freqs=np.linspace(0,250,(250*3)+1,endpoint=True)
         columns= [i for i, x in enumerate((freqs>=self.in_between[0]) & (freqs<self.in_between[1])) if x]
         for win in tqdm(self.timeWindows):
-            main=pd.read_csv(self.path+str(self.case)+'_250_PSD/'+win,header=None)
+            main=pd.read_csv(self.path+str(self.effectSize)+'_250_PSD/'+win,header=None)
             self.main=main[main.columns[columns]].iloc[np.concatenate((self.idx,self.idx+250))]
             Dataframe=pd.concat([Dataframe,self.main],ignore_index=False)
         Dataframe.reset_index(inplace=True)
@@ -73,13 +73,39 @@ class Generate_Data:
         self.Dataframe=Dataframe
         self.columns=columns
         self.freqs=freqs[columns]
-     # def 
+    
+    def plot_timeSeries(self):
+        freqs=np.linspace(0,250,(250*3)+1,endpoint=True)
+        Dataframe=pd.DataFrame()
+        columns= [i for i, x in enumerate((freqs>=self.in_between[0]) & (freqs<self.in_between[1])) if x]
+        freqs=freqs[columns]
+        for file in self.Dir:
+            main=pd.read_csv(self.path+str(self.effectSize)+'_250_PSD/'+file,header=None)
+            main=main[main.columns[columns]]
+            Dataframe=pd.concat([Dataframe,main],ignore_index=False)
+        Dataframe.reset_index(inplace=True)
+        Dataframe.rename({'index':'id'},axis=1,inplace=True)
+        Dataframe=Dataframe.groupby('id').mean()
+
+        PSD=np.array(Dataframe)
+        meanA=np.mean(PSD[:int(PSD.shape[0]/2)],axis=0)
+        meanB=np.mean(PSD[int(PSD.shape[0]/2):],axis=0)
+        stdA=np.std(PSD[:int(PSD.shape[0]/2)],axis=0)
+        stdB=np.std(PSD[int(PSD.shape[0]/2):],axis=0)
+        plt.plot(freqs,meanA,'r')
+        plt.plot(freqs,meanB,'g')
+        plt.fill_between(freqs,meanA+stdA,meanA-stdA,alpha=.5,color='r')
+        plt.fill_between(freqs,meanB+stdB,meanB-stdB,alpha=.5,color='g')
+        plt.title('Band= '+Generate_Data.band+', Mean A vs B groups, all widows, all sujects. Freqs between'+str(self.in_between))
+        plt.show()
+            
      
 class Periodic_Aperiodic:
     
     def __init__(self):
         self.periodic=pd.DataFrame()
         self.aperiodic=pd.DataFrame()
+        self.freqs=[]
     
     def fooof(self,Data):
         inBetween=Data.in_between
@@ -88,11 +114,12 @@ class Periodic_Aperiodic:
         df.drop(columns=['Cohort'],inplace=True)
         periodic= copy.copy(df)
         aperiodic=copy.copy(df)
+        self.freqs=Data.freqs
         parameters=[]
         for i in tqdm(range(df.shape[0])):
             fm = FOOOF(max_n_peaks=1, aperiodic_mode='fixed')
-            fm.add_data(Data.freqs, np.array(df.iloc[i]),inBetween)
-            fm.fit(freqs, np.array(df.iloc[i]), inBetween)
+            fm.add_data(self.freqs, np.array(df.iloc[i]),inBetween)
+            fm.fit(self.freqs, np.array(df.iloc[i]), inBetween)
             periodic.iloc[i]=fm._peak_fit
             aperiodic.iloc[i]=fm._ap_fit
             exp = fm.get_params('aperiodic_params', 'exponent')
@@ -101,11 +128,15 @@ class Periodic_Aperiodic:
             pws = fm.get_params('peak_params', 'PW')
             bws = fm.get_params('peak_params', 'BW')
             parameters.append([exp,offset,cfs,pws,bws])
+            # parameters.append([cfs,pws,bws])
         periodic['Cohort']=cohort
         aperiodic['Cohort']=cohort
         parameters=np.array(parameters)
         parameters=np.concatenate((parameters,np.array([cohort]).T),axis=1)
-        parameters=pd.DataFrame(parameters, columns=['exp','offset','cfs','pws','bws','Cohort'])
+        band=Generate_Data.band
+        parameters=pd.DataFrame(parameters, columns=[band+'exp',band+'offset',band+'cfs',band+'pws',band+'bws','Cohort'])
+        # parameters=pd.DataFrame(parameters, columns=[band+'cfs',band+'pws',band+'bws','Cohort'])
+
         self.periodic=periodic
         self.aperiodic=aperiodic
         self.parameters=parameters
@@ -128,22 +159,24 @@ class Periodic_Aperiodic:
         meanB=np.mean(PSD[int(PSD.shape[0]/2):],axis=0)
         stdA=np.std(PSD[:int(PSD.shape[0]/2)],axis=0)
         stdB=np.std(PSD[int(PSD.shape[0]/2):],axis=0)
-        plt.plot(freqs,meanA,'r')
-        plt.plot(freqs,meanB,'g')
-        plt.fill_between(freqs,meanA+stdA,meanA-stdA,alpha=.5,color='r')
-        plt.fill_between(freqs,meanB+stdB,meanB-stdB,alpha=.5,color='g')
+        plt.plot(self.freqs,meanA,'r')
+        plt.plot(self.freqs,meanB,'g')
+        plt.fill_between(self.freqs,meanA+stdA,meanA-stdA,alpha=.5,color='r')
+        plt.fill_between(self.freqs,meanB+stdB,meanB-stdB,alpha=.5,color='g')
+        plt.title('Band= '+Generate_Data.band+', '+component+' Components')
         plt.show()
     
     def boxplot_coeffs (self):
         Par=self.parameters
+        band=Generate_Data.band
         fig, ax = plt.subplots(1, 5, figsize=(10, 6))
-        sns.boxplot( x=Par['Cohort'], y=Par['exp'],ax=ax[0] )
-        sns.boxplot( x=Par['Cohort'], y=Par['offset'],ax=ax[1] )
-        sns.boxplot( x=Par['Cohort'], y=Par['cfs'],ax=ax[2] )
-        sns.boxplot( x=Par['Cohort'], y=Par['pws'],ax=ax[3] )
-        sns.boxplot( x=Par['Cohort'], y=Par['bws'],ax=ax[4] )
-        plt.subplots_adjust(wspace=0.5) 
-
+        sns.boxplot( x=Par['Cohort'], y=Par[band+'exp'],ax=ax[0] )
+        sns.boxplot( x=Par['Cohort'], y=Par[band+'offset'],ax=ax[1] )
+        sns.boxplot( x=Par['Cohort'], y=Par[band+'cfs'],ax=ax[2] )
+        sns.boxplot( x=Par['Cohort'], y=Par[band+'pws'],ax=ax[3] )
+        sns.boxplot( x=Par['Cohort'], y=Par[band+'bws'],ax=ax[4] )
+        # plt.subplots_adjust(wspace=0.5) 
+        plt.suptitle('Aperiodic and Periodic coeff, band=' +Generate_Data.band)
         plt.show()
         
         
@@ -152,48 +185,45 @@ class Periodic_Aperiodic:
     # def get_df(self,Data):
     #     return Data.Dataframe
 
-           
+def GetStuff(band,Windows,sampleSize,seed):     
+    Generate_Data.band=band
+    Data=Generate_Data()
+    # Data.get_path()
+    Data.Open_effectSize()
+    Data.plot_timeSeries()
+    Data.Generate_Window_idx(Windows=Windows, sampleSize=sampleSize, seed=seed)
+    Data.readCSV_and_Append()
+    APer=Periodic_Aperiodic()
+    APer.fooof(Data)
+    Per=APer.get_Periodic()
+    Ap=APer.get_Aperiodic()
+    Par=APer.get_Parameters()
+    APer.plot_parameters('periodic')
+    APer.plot_parameters('aperiodic')
+    APer.boxplot_coeffs()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection = '3d')
+    x=np.array(Par.loc[:,band+'cfs'])
+    y=np.array(Par.loc[:,band+'pws'])
+    z=np.array(Par.loc[:,band+'bws'])
+    ax.set_xlabel(band+'cfs')
+    ax.set_ylabel(band+'pws')
+    ax.set_zlabel(band+'bws')
+    color=Par.loc[:,'Cohort']
+    ax.scatter(x,y,z,c=color,alpha=.5)
+    plt.show()
+    return Data,APer,Per,Ap, Par
 
-# case1=Generate_Data()
-# Dir1=case1.OpenCase()
-# case1.Generate_Window_idx()
+plt.close('all')
+Data_Alpha,APer_Alpha,Per_Alpha,Ap_Alpha, Par_Alpha = GetStuff('alpha', Windows=50, sampleSize=250,seed=2)
+Data_Beta,APer_Beta,Per_Beta,Ap_Beta, Par_Beta = GetStuff('beta', Windows=50, sampleSize=250,seed=2)
 
-# Generate_Data.band='alpha'
-# Generate_Data.in_between=[1]
-Generate_Data.band='beta'
-Data=Generate_Data()
-Data.get_path()
-Data.OpenCase()
-Data.Generate_Window_idx(Windows=30, sampleSize=250)
-Data.readCSV_and_Append()
+AlphaBetaCoeffDf=pd.concat([Par_Alpha.iloc[:,:-1],Par_Beta],axis=1)
 
-DataFrame=Data.Dataframe
-freqs=Data.freqs
-columns=Data.columns
-main=Data.main
-idx=Data.idx
-
-index,count=np.unique(DataFrame.index,return_counts=True)
-
-APer=Periodic_Aperiodic()
-APer.fooof(Data)
-Per=APer.get_Periodic()
-Ap=APer.get_Aperiodic()
-Par=APer.get_Parameters()
-APer.plot_parameters('periodic')
-APer.plot_parameters('aperiodic')
-APer.boxplot_coeffs()
+#clean nans
+drop=[i for i,x in enumerate(AlphaBetaCoeffDf.isna().any(axis=1)) if x]
+AlphaBetaCoeffDf.drop(drop,inplace=True)
 
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection = '3d')
-x=np.array(Par.loc[:,'cfs'])
-y=np.array(Par.loc[:,'pws'])
-z=np.array(Par.loc[:,'bws'])
-ax.set_xlabel('cfs')
-ax.set_ylabel('pws')
-ax.set_zlabel('bws')
-color=Par.loc[:,'Cohort']
-ax.scatter(x,y,z,c=color,alpha=.5)
 
     
---
+++ PCA_test.py
@@ -0,0 +1,133 @@
+# ==============================================================================
+from sklearn.decomposition import PCA
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+def ACP (DataFrame):
+    print('----------------------')
+    print('Media de cada variable')
+    print('----------------------')
+    print(DataFrame.mean(axis=0))
+    
+    
+    print('-------------------------')
+    print('Varianza de cada variable')
+    print('-------------------------')
+    print(DataFrame.var(axis=0))
+    
+    #Etiqueta de componentes a calcular
+    # ==========================================================================
+    Etiquetas=[]
+    for i in range(len(DataFrame.keys())):
+        Etiquetas.append('PC'+str(i+1))
+    
+    # Entrenamiento modelo PCA con escalado de los datos
+    # ==============================================================================
+    pca_pipe = make_pipeline(StandardScaler(), PCA())
+    pca_pipe.fit(DataFrame)
+    
+    # Se extrae el modelo entrenado del pipeline
+    modelo_pca = pca_pipe.named_steps['pca']
+    
+    dfPca=pd.DataFrame(
+        data    = modelo_pca.components_,
+        columns = DataFrame.columns,
+        index   = Etiquetas
+    )
+    
+    print(dfPca)
+    
+    # Heatmap componentes
+    # ==============================================================================
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(4, 2))
+    componentes = modelo_pca.components_
+    plt.imshow(componentes.T, cmap='viridis', aspect='auto')
+    plt.yticks(range(len(DataFrame.columns)), DataFrame.columns)
+    plt.xticks(range(len(DataFrame.columns)), np.arange(modelo_pca.n_components_) + 1)
+    plt.grid(False)
+    plt.colorbar();
+    
+    
+    # Porcentaje de varianza explicada por cada componente
+    # ==============================================================================
+    print('----------------------------------------------------')
+    print('Porcentaje de varianza explicada por cada componente')
+    print('----------------------------------------------------')
+    print(modelo_pca.explained_variance_ratio_)
+    
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 4))
+    ax.bar(
+        x      = np.arange(modelo_pca.n_components_) + 1,
+        height = modelo_pca.explained_variance_ratio_
+    )
+    
+    for x, y in zip(np.arange(len(DataFrame.columns)) + 1, modelo_pca.explained_variance_ratio_):
+        label = round(y, 2)
+        ax.annotate(
+            label,
+            (x,y),
+            textcoords="offset points",
+            xytext=(0,10),
+            ha='center'
+        )
+    
+    ax.set_xticks(np.arange(modelo_pca.n_components_) + 1)
+    ax.set_ylim(0, 1.1)
+    ax.set_title('Porcentaje de varianza explicada por cada componente')
+    ax.set_xlabel('Componente principal')
+    ax.set_ylabel('Por. varianza explicada');
+    
+    # Porcentaje de varianza explicada acumulada
+    # ==============================================================================
+    prop_varianza_acum = modelo_pca.explained_variance_ratio_.cumsum()
+    print('------------------------------------------')
+    print('Porcentaje de varianza explicada acumulada')
+    print('------------------------------------------')
+    print(prop_varianza_acum)
+    
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 4))
+    ax.plot(
+        np.arange(len(DataFrame.columns)) + 1,
+        prop_varianza_acum,
+        marker = 'o'
+    )
+    
+    for x, y in zip(np.arange(len(DataFrame.columns)) + 1, prop_varianza_acum):
+        label = round(y, 2)
+        ax.annotate(
+            label,
+            (x,y),
+            textcoords="offset points",
+            xytext=(0,10),
+            ha='center'
+        )
+        
+    ax.set_ylim(0, 1.1)
+    ax.set_xticks(np.arange(modelo_pca.n_components_) + 1)
+    ax.set_title('Porcentaje de varianza explicada acumulada')
+    ax.set_xlabel('Componente principal')
+    ax.set_ylabel('Por. varianza acumulada');
+    
+    proyecciones = pca_pipe.transform(X=DataFrame)
+    proyecciones = pd.DataFrame(
+        proyecciones,
+        columns = Etiquetas,
+        index   = DataFrame.index
+    )
+    print(proyecciones.head())
+    
+    return proyecciones,prop_varianza_acum
+
+proyecciones,prop_varianza_acum=ACP(AlphaBetaCoeffDf.iloc[:,:-1])
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection = '3d')
+x=np.array(proyecciones.loc[:,'PC1'])
+y=np.array(proyecciones.loc[:,'PC2'])
+z=np.array(proyecciones.loc[:,'PC5'])
+ax.set_xlabel('PC1')
+ax.set_ylabel('PC2')
+ax.set_zlabel('PC5')
+color=AlphaBetaCoeffDf.loc[:,'Cohort']
+ax.scatter(x,y,z,c=color,alpha=.5)
+plt.show()

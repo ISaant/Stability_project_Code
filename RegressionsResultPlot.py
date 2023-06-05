08f5000a7e580ba2bf50ed1c4fb59ea3e98caeb3
ISaant <sifa.sant@gmail.com>
2023-06-05 14:01:36 -0400

    Regression Plot Results .py was modfied based on jasson and alex commentaries

--- RegressionsResultPlot.py
+++ RegressionsResultPlot.py
@@ -11,29 +11,30 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def RegPlot (parentPath):
+def RegPlot (current_path):
 
-    with open(parentPath+'/Pickle/AgePredictions2.pickle', 'rb') as f:
+    with open(current_path+'/Pickle/AgePredictions2.pickle', 'rb') as f:
         Age=pickle.load(f)
         
-    with open(parentPath+'/Pickle/CatellPredictions2.pickle', 'rb') as f:
+    with open(current_path+'/Pickle/CatellPredictions2.pickle', 'rb') as f:
         Catell=pickle.load(f)
         
-    with open(parentPath+'/Pickle/AcerPredictions2.pickle', 'rb') as f:
+    with open(current_path+'/Pickle/AcerPredictions2.pickle', 'rb') as f:
         Acer=pickle.load(f)
         
     # dfAge=pd.DataFrame(columns=['Corr','Algorithm','Num_of_Windows'])
     # dfCatell=pd.DataFrame(columns=['Corr','Algorithm','Num_of_Windows'])
     # dfAcer=pd.DataFrame(columns=['Corr','Algorithm','Num_of_Windows'])
-    
+    #%%
     def mod_df(test): 
-        Df=pd.DataFrame(columns=['Corr','Algorithm','Num_of_Windows'])
+        Df=pd.DataFrame(columns=['Corr','Algorithm','Num_of_Windows [30s]'])
         for i in range(10):
             results=test[i]
             corr=np.reshape(results,(150,),order='F')
             iteration=np.ones(150)+i
             algorithm=np.reshape([['Lasso']*50,['Perceptron']*50,['RandomForest']*50],(150,)).astype(str)
-            df=pd.DataFrame({'Corr':corr,'Algorithm':algorithm,'Num_of_Windows [30s]':iteration})
+            # df=pd.DataFrame({'Corr':corr,'Algorithm':algorithm,'Num_of_Windows [30s]':iteration})
+            df=pd.DataFrame({'Corr':np.arctanh(corr),'Algorithm':algorithm,'Num_of_Windows [30s]':iteration})
             # lcls = locals()
             # exec('df'+target+'=pd.concat([df'+target+',df],ignore_index=True)',globals(),lcls)
             Df=pd.concat([Df,df],ignore_index=True)
@@ -43,16 +44,33 @@ def RegPlot (parentPath):
     dfAcer=mod_df(Acer)
     
     # # plt.close('all')
-    def plotLine(dframe,ylim,title):
-        plt.figure()
-        sns.lineplot(data=dframe,x='Num_of_Windows [30s]',y='Corr',hue='Algorithm', markers=True, palette="flare")
+    def plotLine(dframe,ylim,order,title):
+        
+        def fit_line_poli(dframe, order, ax):
+            windows = np.unique(dframe['Num_of_Windows [30s]']).astype(int)
+            Alg,c=np.unique(dframe['Algorithm'],return_counts=True)
+            algArray=[]
+            for alg in Alg:
+                Means=np.zeros((max(windows)))
+                for j,win in enumerate(windows):
+                    mean=np.mean(dframe.iloc[np.where((dframe['Num_of_Windows [30s]']==win) & (dframe['Algorithm']==alg))[0]]['Corr'])
+                    Means[j]=mean
+                MeansDf=pd.DataFrame({'Corr':Means,'Windows': list(windows)})
+                sns.regplot(x="Windows", y="Corr",data=MeansDf, order=order, scatter=False, color = 'k', ci=False, line_kws=dict(alpha=1,linewidth=1,linestyle='--'),ax=ax)
+            
+                    
+        fig, ax = plt.subplots()
+        sns.lineplot(data=dframe,x='Num_of_Windows [30s]',y='Corr',hue='Algorithm', markers=True, palette="flare", ax=ax)
+        fit_line_poli(dframe, order, ax)
+        plt.ylabel('arctanh(Corr)')
         plt.ylim(ylim)
         plt.title(title)
     
-    
-    plotLine(dfAge,[0,.85],'Age regression')
-    plotLine(dfCatell,[0,.85], 'Catell Score regression')
-    plotLine(dfAcer,[0,.85], 'Acer Score regression')
+    #%%
+    plotLine(dfAge,[0.6,1.2],2,'Age regression')
+    plotLine(dfCatell,[0.2,0.7],2, 'Catell Score regression')
+    plotLine(dfAcer,[0,0.4],2,'Acer Score regression')
 
+    #%%
     return dfAge
   
--- untitled0.py
+++ untitled0.py
@@ -1,12 +1,197 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed May 24 22:05:02 2023
+Created on Fri May 26 18:34:18 2023
 
 @author: isaac
 """
-sns.set(font_scale=1)
-data=pd.read_csv(current_path+'/example4Luc 4/dka_data.csv')
-data.rename(columns={'Acc':'Decoding Corr','network':'YEO'},inplace=True)
-sns.boxplot(data,x='YEO',y='Decoding Corr',palette="viridis").set(title='YEO decoding Correlation Boxplot')#gist_earth_r, mako_r, rocket_r
-plt.xticks(rotation=15, ha='right')
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import hilbert
+
+#%%
+# import numpy as np
+from scipy.signal import firls, filtfilt
+
+def eegfilt(data, srate, locutoff, hicutoff, epochframes=None, filtorder=None, revfilt=None):
+    if len(data.shape) > 1 and data.shape[1] == 1:
+        raise ValueError("Input data should be a row vector.")
+
+    frames = len(data)
+    nyq = srate * 0.5  # Nyquist frequency
+    MINFREQ = 0
+    minfac = 3  # this many (lo)cutoff-freq cycles in filter
+    min_filtorder = 15  # minimum filter length
+    trans = 0.15  # fractional width of transition zones
+
+    if locutoff > 0 and hicutoff > 0 and locutoff > hicutoff:
+        raise ValueError("locutoff > hicutoff")
+
+    if locutoff < 0 or hicutoff < 0:
+        raise ValueError("locutoff or hicutoff < 0")
+
+    if locutoff > nyq:
+        raise ValueError("Low cutoff frequency cannot be > srate/2")
+
+    if hicutoff > nyq:
+        raise ValueError("High cutoff frequency cannot be > srate/2")
+
+    if filtorder is None:
+        filtorder = 0
+
+    if revfilt is None:
+        revfilt = 0
+
+    if filtorder == 0 or filtorder is None:
+        if locutoff > 0:
+            filtorder = minfac * int(srate / locutoff)
+        elif hicutoff > 0:
+            filtorder = minfac * int(srate / hicutoff)
+
+        if filtorder < min_filtorder:
+            filtorder = min_filtorder
+
+    if epochframes is None:
+        epochframes = 0
+
+    if epochframes == 0:
+        epochframes = frames
+
+    epochs = frames // epochframes
+
+    if epochs * epochframes != frames:
+        raise ValueError("epochframes does not divide frames.")
+
+    if filtorder * 3 > epochframes:
+        raise ValueError("epochframes must be at least 3 times the filtorder.")
+
+    if (1 + trans) * hicutoff / nyq > 1:
+        raise ValueError("High cutoff frequency too close to Nyquist frequency")
+
+    if locutoff > 0 and hicutoff > 0:
+        if revfilt:
+            print("eegfilt() - performing {}-point notch filtering.".format(filtorder))
+        else:
+            print("eegfilt() - performing {}-point bandpass filtering.".format(filtorder))
+
+        f = [MINFREQ, (1 - trans) * locutoff / nyq, locutoff / nyq, hicutoff / nyq, (1 + trans) * hicutoff / nyq, 1]
+        m = [0, 0, 1, 1, 0, 0]
+    elif locutoff > 0:
+        print("eegfilt() - performing {}-point highpass filtering.".format(filtorder))
+        f = [MINFREQ, locutoff * (1 - trans) / nyq, locutoff / nyq, 1]
+        m = [0, 0, 1, 1]
+    elif hicutoff > 0:
+        print("eegfilt() - performing {}-point lowpass filtering.".format(filtorder))
+        f = [MINFREQ, hicutoff / nyq, hicutoff * (1 + trans) / nyq, 1]
+        m = [1, 1, 0, 0]
+    else:
+        raise ValueError("You must provide a non-zero low or high cut-off frequency")
+
+    if revfilt:
+        m = np.logical_not(m)
+    
+    print(filtorder,f,m)
+    if filtorder%2 == 1:
+        filtorder+=1
+    filtwts = firls(filtorder+1, f, m)  # get FIR filter coefficients
+
+    smoothdata = np.zeros((frames))
+    for e in range(epochs):  # filter each epoch, channel
+        for c in range(1):
+            smoothdata[e*epochframes:(e+1)*epochframes] = filtfilt(filtwts, 1, data[e*epochframes:(e+1)*epochframes])
+
+            if epochs == 1 and c % 20 != 0:
+                print('.', end='')
+            elif epochs == 1:
+                print(c, end='')
+
+    return smoothdata, filtwts
+
+#%%
+
+
+def ModIndex_v2(Phase, Amp, position):
+    nbin = len(position)
+    winsize = 2 * np.pi / nbin
+
+    MeanAmp = np.zeros(nbin)
+    for j in range(nbin):
+        I = np.where((Phase < position[j] + winsize) & (Phase >= position[j]))
+        MeanAmp[j] = np.mean(Amp[I])
+
+    MI = (np.log(nbin) - (-np.sum((MeanAmp / np.sum(MeanAmp)) * np.log((MeanAmp / np.sum(MeanAmp)))))) / np.log(nbin)
+
+    return MI, MeanAmp
+
+#%%
+
+# Constructing an example
+data_length = 2**15
+srate = 1024
+dt = 1 / srate
+t = dt * np.arange(1, data_length + 1)
+
+nonmodulatedamplitude = 2
+Phase_Modulating_Freq = 10
+Amp_Modulated_Freq = 80
+
+lfp = (0.2 * (np.sin(2 * np.pi * t * Phase_Modulating_Freq) + 1) + nonmodulatedamplitude * 0.1) * np.sin(
+    2 * np.pi * t * Amp_Modulated_Freq) + np.sin(2 * np.pi * t * Phase_Modulating_Freq)
+lfp = lfp + 1 * np.random.randn(len(lfp))
+
+# Plotting the signal
+plt.figure()
+plt.subplot(2, 1, 1)
+plt.plot(t, lfp)
+plt.xlim([0, 1])
+plt.xlabel('time (s)')
+plt.ylabel('mV')
+
+# Define the amplitude- and phase-frequencies
+PhaseFreqVector = np.arange(2, 51, 2)
+AmpFreqVector = np.arange(10, 201, 5)
+PhaseFreq_BandWidth = 4
+AmpFreq_BandWidth = 20
+
+# Define phase bins
+nbin = 18
+position = np.linspace(-np.pi, np.pi - 2 * np.pi / nbin, nbin)
+
+# Filtering and Hilbert transform
+Comodulogram = np.zeros((len(PhaseFreqVector), len(AmpFreqVector)))
+AmpFreqTransformed = np.zeros((len(AmpFreqVector), data_length))
+PhaseFreqTransformed = np.zeros((len(PhaseFreqVector), data_length))
+
+for ii, Af1 in enumerate(AmpFreqVector):
+    Af2 = Af1 + AmpFreq_BandWidth
+    AmpFreq = lfp
+    # Perform filtering
+    AmpFreq=eegfilt(lfp,srate,Af1,Af2)[0]
+    # AmpFreq = filtfilt(b, a, AmpFreq)
+    AmpFreqTransformed[ii, :] = np.abs(hilbert(AmpFreq))  # Getting the amplitude envelope
+
+for jj, Pf1 in enumerate(PhaseFreqVector):
+    Pf2 = Pf1 + PhaseFreq_BandWidth
+    PhaseFreq = lfp
+    # Perform filtering
+    PhaseFreq=eegfilt(lfp,srate,Pf1,Pf2)[0]
+    PhaseFreqTransformed[jj, :] = np.angle(hilbert(PhaseFreq))  # Getting the phase time series
+
+# Compute MI and comodulogram
+Comodulogram = np.zeros((len(PhaseFreqVector), len(AmpFreqVector)))
+
+for ii, Pf1 in enumerate(PhaseFreqVector):
+    Pf2 = Pf1 + PhaseFreq_BandWidth
+    for jj, Af1 in enumerate(AmpFreqVector):
+        Af2 = Af1 + AmpFreq_BandWidth
+        MI, MeanAmp = ModIndex_v2(PhaseFreqTransformed[ii, :], AmpFreqTransformed[jj, :], position)
+        Comodulogram[ii, jj] = MI
+
+# Plot comodulogram
+plt.figure()
+plt.contourf(PhaseFreqVector + PhaseFreq_BandWidth / 2, AmpFreqVector + AmpFreq_BandWidth / 2, Comodulogram.T, 30, lines='none')
+plt.xlabel('Phase Frequency (Hz)')
+plt.ylabel('Amplitude Frequency (Hz)')
+plt.colorbar()
+plt.show()
